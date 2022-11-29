be22051ed3c4985333df332fcb625d6cf4968707
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2022-11-30 00:29:28 +0100

    install sidekiq

--- app/jobs/fake_job.rb
+++ app/jobs/fake_job.rb
@@ -1,7 +1,9 @@
 class FakeJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform
+    puts "Starting the fake job"
+    sleep(3) # Calling an heavy API for instance
+    puts "The job is done"
   end
 end
