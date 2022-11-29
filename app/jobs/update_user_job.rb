ae0f2a7e057aeaad5e0ab7a89b60d0d72e705b3e
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2022-11-30 00:51:58 +0100

    generate task

--- app/jobs/update_user_job.rb
+++ app/jobs/update_user_job.rb
@@ -1,7 +1,10 @@
 class UpdateUserJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(user)
+    puts "Updating the user with email - #{user.email}..."
+    # TODO: perform a time consuming task like Clearbit's Enrichment API.
+    sleep 2
+    puts "Done! Updated #{user.email}"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  after_commit :async_update
+
+  private
+
+  def sync_update
+    UpdateUserJob.perform_now(self)
+  end
+
+  def async_update
+    UpdateUserJob.perform_later(self)
+  end
 end
